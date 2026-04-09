d96a0e22c3b22716b5b56e985316ed7897f2c715
chazyua <art.t35t@gmail.com>
2026-04-09 13:39:08 -0700

    add markdown module public api

---
+++ src/markdown/index.ts
@@ -0,0 +1,5 @@
+export { blocksToMarkdown } from "./read.js";
+export { markdownToBlocks } from "./write.js";
+export { richTextToMarkdown, markdownToRichText } from "./tokenizer.js";
+export type { Block, RichText, Annotations, BlockType } from "./types.js";
+export { DEFAULT_ANNOTATIONS } from "./types.js";
